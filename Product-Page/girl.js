04b4cf2fda2510e09009b52eb6c49c58e5fde150
Pintu Kumar <kushwahapintu903@gmail.com>
2022-05-06 11:20:15 +0530

    80% has done

---
+++ Product-Page/girl.js
@@ -0,0 +1,327 @@
+var mensData=[
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/16827542/2022/3/21/1751593d-49f1-425b-995f-103d03d5ae251647855800571-Bella-Moda-Green-Floral-Embroidered-Dress-5481647855800504-2.jpg",
+        brand:"Bella Moda",
+        price:"499",
+        off:"(90%OFF)",
+        pprice:"2299",
+        color:"green",
+        name:"Floral Embroidered Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/17331488/2022/3/1/f4633269-a57e-416a-96e0-8e386c93b3d11646074043113AarikaPinkOff-ShoulderNetMidiDress1.jpg",
+        brand:"Aarika",
+        price:"999",
+        off:"(67%OFF)",
+        pprice:"2699",
+        color:"black",
+        name:"Net Midi Off-shoulder Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/6/3/46202efb-a468-41c1-bab3-9d6466b553d01622694977178-1.jpg",
+        brand:"Wish Karo",
+        price:"1549",
+        off:"(40%OFF)",
+        pprice:"2873",
+        color:"pink",
+        name:"Colourblocked Net Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/17651562/2022/3/28/884025cf-02c3-423a-b7ac-1b313117c3811648461068492BellaModaRedFloralDress1.jpg",
+        brand:"Bella Moda",
+        price:"399",
+        off:"(95%OFF)",
+        pprice:"1999",
+        color:"red",
+        name:"Girls Fit Flared Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/14550060/2021/7/15/0e2b86b5-73f5-4f10-8f13-335accfa660e1626347938457CUTECUMBERBlackSequinedGeorgetteFitandFlareDress1.jpg",
+        brand:"CUTECUMBER",
+        price:"789",
+        off:"(59%OFF)",
+        pprice:"1799",
+        color:"black",
+        name:"Sequined Fit and Flared Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/17273788/2022/2/24/27084ac7-7118-401a-b1cc-a8fb552c351c1645705049816CUTECUMBERRedCheckedGeorgetteSheathDress1.jpg",
+        brand:"CUTECUMBER",
+        price:"1390",
+        off:"(25%OFF)",
+        pprice:"1800",
+        color:"red",
+        name:"Checked Georgette Sheath Dre..",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/6/29/d15cccee-d064-458e-8e09-c9aed44b1ded1624958826140-1.jpg",
+        brand:"Wish Karo",
+        price:"2999",
+        off:"(60%OFF)",
+        pprice:"4499",
+        color:"white",
+        name:"Baby Girls Flora; Satin Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/15583966/2022/1/27/e2303415-65f9-4839-b67b-862993b48e9e1643279926256-Bella-Moda-Black-Georgette-Dress-7631643279926113-1.jpg",
+        brand:"Bella Moda",
+        price:"799",
+        off:"(65%OFF)",
+        pprice:"1799",
+        color:"black",
+        name:"Georgette Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/17844398/2022/4/11/2667f7e0-7e9b-4d12-927c-33bbbe92c9b51649658757793AarikaTurquoiseBlueNetDress1.jpg",
+        brand:"Aarika",
+        price:"859",
+        off:"(60%OFF)",
+        pprice:"2399",
+        color:"blue",
+        name:"Net Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/16025662/2021/11/8/4668ae2f-cadc-4762-ab96-a4d5e81b6eec1636370379154Dresses1.jpg",
+        brand:"Wish Karo",
+        price:"999",
+        off:"(61%OFF)",
+        pprice:"2499",
+        color:"pink",
+        name:"Girls Satin Dress",
+        type:"girlkid"
+    },   
+     {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/2/18/f5200f52-83cf-4927-851b-d46e7548551e1613651566819-1.jpg",
+        brand:"CUTECUMBER",
+        price:"2999",
+        off:"(43%OFF)",
+        pprice:"5599",
+        color:"pink",
+        name:"Girls Printed A-Line Dress",
+        type:"girlkid"
+    },
+    {
+        image_url:"https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/17659796/2022/3/30/cbab1b98-81fd-4c42-87a9-2c4a1d3dba0b1648584929651AarikaMaroonEmbellishedNetMaxiDress1.jpg",
+        brand:"PUMA",
+        price:"751",
+        off:"(55%OFF)",
+        pprice:"1559",
+        color:"red",
+        name:"Girls Embellished Net Maxi Dr..",
+        type:"girlkid"
+    }
+]
+
+displaydata(mensData)
+
+document.querySelector("#top > p").addEventListener("click",clear)
+function clear(){
+    window.location.reload()
+}
+//                                                       Brand Code
+
+document.querySelector("#road").addEventListener("click",roadster)
+function roadster(){
+   var a=mensData.filter(function(elem){
+       return elem.brand=="Bella Moda"
+   })
+      displaydata(a)
+   }
+
+document.querySelector("#wro").addEventListener("click",wrogen)
+   function wrogen(){
+   var a=mensData.filter(function(elem){
+       return elem.brand=="Aarika"
+   })
+   displaydata(a)
+   }
+
+   document.querySelector("#arrow").addEventListener("click",arrow)
+   function arrow(){
+   var a=mensData.filter(function(elem){
+       return elem.brand=="PUMA"
+   })
+   displaydata(a)
+   }
+
+   document.querySelector("#dennis").addEventListener("click",dennis)
+   function dennis(){
+   var a=mensData.filter(function(elem){
+       return elem.brand=="Wish Karo"
+   })
+   displaydata(a)
+   }
+
+   document.querySelector("#high").addEventListener("click",highlander)
+   function highlander(){
+   var a=mensData.filter(function(elem){
+       return elem.brand=="CUTECUMBER"
+   })
+   displaydata(a)
+   }
+
+//                                                           Color Code
+
+
+   document.querySelector("#black").addEventListener("click",black)
+   function black(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="black"
+    })
+    displaydata(a)
+
+   }
+   document.querySelector("#pink").addEventListener("click",pink)
+   function pink(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="pink"
+    })
+    displaydata(a)
+   }
+   document.querySelector("#white").addEventListener("click",white)
+   function white(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="white"
+    })
+    displaydata(a)
+   }
+   document.querySelector("#green").addEventListener("click",green)
+   function green(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="green"
+    })
+    displaydata(a)
+   }
+   document.querySelector("#red").addEventListener("click",red)
+   function red(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="red"
+    })
+    displaydata(a)
+   }
+   document.querySelector("#blue").addEventListener("click",blue)
+   function blue(){
+       var a=mensData.filter(function(elem){
+        return elem.color=="blue"
+    })
+    displaydata(a)
+   }
+//                                                        Money Calculation
+
+var a=[]
+var b=[]
+var c=[]
+var d=[]
+
+for(var i=0; i<mensData.length; i++)
+{
+    if(mensData[i].price >=159 && mensData[i].price < 549)
+    {
+        a.push(mensData[i])
+    }
+    if(mensData[i].price >=549 && mensData[i].price < 1199)
+    {
+        b.push(mensData[i])
+    }
+    if(mensData[i].price >=1199 && mensData[i].price <1999)
+    {
+        c.push(mensData[i])
+    }
+    if(mensData[i].price >=1999 && mensData[i].price <3000)
+    {
+        d.push(mensData[i])
+    }
+}
+
+document.querySelector("#start").addEventListener("click",startfun)
+function startfun(){
+    var w=a.filter(function(elem){
+        return elem;
+    })
+    displaydata(w)
+}
+document.querySelector("#medium").addEventListener("click",startfun2)
+function startfun2(){
+    var w=b.filter(function(elem){
+        return elem;
+    })
+    displaydata(w)
+}
+document.querySelector("#end").addEventListener("click",startfun3)
+function startfun3(){
+    var w=c.filter(function(elem){
+        return elem;
+    })
+    displaydata(w)
+}
+document.querySelector("#end2").addEventListener("click",startfun4)
+function startfun4(){
+    var w=d.filter(function(elem){
+        return elem;
+    })
+    displaydata(w)
+}
+
+//                                              Display Data
+
+function displaydata(mensData){
+
+    document.querySelector("#container").innerHTML=""
+    mensData.forEach(function(elem){
+  
+
+        var div=document.createElement("div")
+   
+        var image=document.createElement("img")
+        image.src=elem.image_url
+
+        image.addEventListener("click",imageopen)
+   
+        var name=document.createElement("p")
+        name.setAttribute("class","name")
+        name.innerText=elem.name
+        
+        var price=document.createElement("h4")
+        price.setAttribute("class","price1")
+        price.innerText="Rs."+ elem.price
+   
+   
+        var of=document.createElement("span")
+        of.setAttribute("class","off")
+        of.innerText=elem.off
+   
+       var s=document.createElement("span")
+       s.setAttribute("class","s")
+       s.innerText="Rs." + elem.pprice
+       price.append(s,of)
+   
+       var col=document.createElement("p")
+       col.setAttribute("class","color")
+
+   
+        var strike=document.createElement("h4")
+        strike.setAttribute("class","strike")
+        strike.innerText=elem.brand
+   
+        div.append(image,strike,name,price)
+        document.querySelector("#container").append(div)
+        
+     })
+
+     function imageopen(){
+        window.location.href="https://www.myntra.com/shirts/roadster/roadster-men-black--grey-checked-casual-sustainable-shirt/1376577/buy"
+     }
+
+   
+}
+
+
